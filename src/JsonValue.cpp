47158bd3a1dceca6f80c5ea24554ffb65157ff39
kyo <approx.solution@gmail.com>
2015-06-30 09:12:54 +0900

    delete the link with msgpack by pragma
    delete the link with msgpack by pragma

--- src/JsonValue.cpp
+++ src/JsonValue.cpp
@@ -5,33 +5,6 @@
  * @date   2015/05/05
  */
 
-//----- MessagePack -----//
-/*
-   Msgpack is Copyright (C) 2008-2014 FURUHASHI Sadayuki
-   and licensed under the Apache License, Version 2.0, as follows.
-
-   ------------------------------------------------------------------------
-   Copyright (C) 2008-2010 FURUHASHI Sadayuki
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-   ------------------------------------------------------------------------
- */
-#if defined(DEBUG) || defined(_DEBUG)
-#pragma comment(lib, "msgpackd.lib") // Debug”Å
-#else
-#pragma comment(lib, "msgpack.lib") // Release”Å
-#endif
-
 #include "JsonValue.h"
 #include "JsonValueString.h"
 #include "JsonValueInt.h"
